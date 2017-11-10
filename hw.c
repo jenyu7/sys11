6cff0117d94dc87d91571b7abd3fa822128f7297
Jen Yu <jyu7@stuy.edu>
2017-11-10 15:31:42 -0500

    doesn't work

--- hw.c
+++ hw.c
@@ -4,3 +4,33 @@
  * HW #11: Don't Get Argumentative
  * 2017-11-10
  */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char ** parse_args( char * line )
+{
+  char ** args = (char **)malloc(sizeof(char **));
+  int i = 0;
+  while(line)
+    {
+      printf("here\n");
+      args[i] = strsep(&line, " ");
+      printf("now here \n");
+      i ++;
+    }
+  return args;
+}
+
+int main()
+{
+  /****website-test-code****
+  char line[100] = "wow-this-is-cool";
+  char *s1 = line;
+  printf("[%s]\n", strsep( &s1, "-" ));
+  printf("[%s]\n", s1);
+  ********/
+  parse_args("ls -a -l");
+  return 0;
+}
